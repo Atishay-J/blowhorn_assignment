ebd1918f05869ea6e689c8d4659ae70bbfec713f
Atishay Jain <hritikjain707@gmail.com>
2021-08-27 15:06:16 +0530

    Refactor Chore: Removed comments and refactored

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,58 +1,8 @@
 import styles from "./blogs.module.css";
 import HorizontalBlogCard from "./Cards/HorizontalBlogCard";
 import VerticalBlogCard from "./Cards/VerticalBlogCard";
+import { horizontalBlogs, verticalBlogs } from "./BlogsData";
 export default function Blogs() {
-  const verticalBlogs = [
-    {
-      image:
-        "https://images.unsplash.com/photo-1606914501491-7b11d54f232f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-      tag: "RESOURCES",
-      title: "Refreshing Designs",
-      info: "Quench satifying designs to help you stir up emotion and tell a story.",
-      contrastColor: "#43d330",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1557800636-894a64c1696f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=401&q=80",
-      tag: "LIFESTYLE",
-      title: "Healthier Lifestyle",
-      info: "Living a healthier lifestyle will help with your productivity and your mind-set.",
-      contrastColor: "#60a5fa",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1592839719941-8e2651039d01?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=453&q=80",
-      tag: "ENTERTAINMENT",
-      title: "Gaming Evolution",
-      info: "Learn about revolution of gaming and how it started",
-      contrastColor: "#a92222",
-    },
-  ];
-
-  const horizontalBlogs = [
-    {
-      image:
-        "https://images.unsplash.com/photo-1597260469329-e1201e075d82?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
-      tag: "INSPIRATION",
-      title: "Best Workstation of the year",
-      info: "Check out these inspiring workstaions to get ideas on how to level-up your workstation.",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80",
-      tag: "FOOD",
-      title: "Eating for Productivity",
-      info: "Learn how to be more disciplined in your diet and how you can eat to maximize productivity",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1587355760421-b9de3226a046?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80",
-      tag: "RESOURCES",
-      title: "A Design Mind-set",
-      info: "What does it mean to have a design mind-set? Learn how to improve your eye for design",
-    },
-  ];
-
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>The Project Blog</h1>
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,73 +1,7 @@
 import styles from "./team.module.css";
 import TeamCard from "./TeamCard";
+import { teamMembers } from "./TeamData";
 export default function Team() {
-  const teamMembers = [
-    {
-      image:
-        "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      name: "Freddy Smith",
-      title: "CEO and Founder",
-      twitter: "https://www.twitter.com",
-      github: "https://www.github.com",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-      name: "Carl Jones",
-      title: "CTO and Co-Founder",
-      twitter: "https://www.twitter.com",
-      github: "https://www.github.com",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-      name: "Susan Peterson",
-      title: " Marketing Direcotry",
-      twitter: "https://www.twitter.com",
-      github: "https://www.github.com",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/1024311/pexels-photo-1024311.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      name: "Tommy Barnes",
-      title: "Designer",
-      twitter: "https://www.twitter.com",
-      github: "https://www.github.com",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1567532939604-b6b5b0db2604?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-      name: "Ron Jenson",
-      title: "Senior Developer",
-      twitter: "https://www.twitter.com",
-      github: "https://www.github.com",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/3812011/pexels-photo-3812011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      name: "Pete Thompkins",
-      title: "Web Developer",
-      twitter: "https://www.twitter.com",
-      github: "https://www.github.com",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1528892952291-009c663ce843?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=296&q=80",
-      name: "Kelly Richards",
-      title: "Sales Manager",
-      twitter: "https://www.twitter.com",
-      github: "https://www.github.com",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1450297350677-623de575f31c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-      name: "Alexis Jordan",
-      title: "Affiliate Manager",
-      twitter: "https://www.twitter.com",
-      github: "https://www.github.com",
-    },
-  ];
-
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>OUR TEAM</h1>
