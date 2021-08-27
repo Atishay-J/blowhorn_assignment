d7ce311e48f80c4e6aac9aff98555a41537c1870
Atishay Jain <hritikjain707@gmail.com>
2021-08-27 15:00:07 +0530

    Fix: Navbar style

--- src/components/Navbar/BurgerMenu.jsx
+++ src/components/Navbar/BurgerMenu.jsx
@@ -5,31 +5,6 @@ import { useState } from "react";
 export default function BurgerMenu() {
   const [showModal, setShowModal] = useState(false);
 
-  //   var modal = document.getElementById("myModal");
-
-  //   // Get the button that opens the modal
-  //   var btn = document.getElementById("myBtn");
-
-  //   // Get the <span> element that closes the modal
-  //   var span = document.getElementsByClassName("close")[0];
-
-  //   // When the user clicks the button, open the modal
-  //   btn.onclick = function () {
-  //     modal.style.display = "block";
-  //   };
-
-  //   // When the user clicks on <span> (x), close the modal
-  //   span.onclick = function () {
-  //     modal.style.display = "none";
-  //   };
-
-  //   // When the user clicks anywhere outside of the modal, close it
-  //   window.onclick = function (event) {
-  //     if (event.target == modal) {
-  //       modal.style.display = "none";
-  //     }
-  //   };
-
   function closeMenu(event) {
     event.preventDefault();
     if (event.target === event.currentTarget) {
@@ -38,8 +13,8 @@ export default function BurgerMenu() {
   }
 
   return (
-    <>
-      <Search />
+    <div className={styles.burgerWrapper}>
+      <Search className={styles.searchIcon} />
       <List
         className={styles.burgerIcon}
         onClick={() => setShowModal((prev) => !prev)}
@@ -50,17 +25,19 @@ export default function BurgerMenu() {
         onClick={(event) => closeMenu(event)}
         style={{ display: showModal ? "block" : "none" }}
       >
-        <ul className={styles.burgerMenu}>
+        <div className={styles.menuWrapper}>
           <XLg
             className={styles.closeBtn}
             onClick={() => setShowModal((prev) => !prev)}
           />
-          <li className={styles.menuItems}>Home</li>
-          <li className={styles.menuItems}>Features</li>
-          <li className={styles.menuItems}>Blog</li>
-          <li className={styles.menuItems}>Contact</li>
-        </ul>
+          <ul className={styles.burgerMenu}>
+            <li className={styles.menuItems}>Home</li>
+            <li className={styles.menuItems}>Features</li>
+            <li className={styles.menuItems}>Blog</li>
+            <li className={styles.menuItems}>Contact</li>
+          </ul>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
